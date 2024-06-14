7057fab2eda27ecc961951ee34fdbae12f7fc178
nadao-1707 <princeegypt28@gmail.com>
2024-06-14 16:20:24 +0300

    ...

--- cosmo_care/lib/Services/AdminController.dart
+++ cosmo_care/lib/Services/AdminController.dart
@@ -8,14 +8,33 @@ class AdminController {
   // to add a new Product data
   Future<void> addProduct({required Product product}) async {
     try {
-      await FirebaseFirestore.instance
-          .collection('Products')
-          .doc()
-          .set(product.toFirestore());
+      await FirebaseFirestore.instance.collection('Products').doc().set(product.toFirestore());
       return; 
     } catch (error) {
       print(error.toString());
       return;
     }
   }
+
+
+  // to delete a product using the product name
+  Future<void> deleteProduct(String name) async {
+    try {
+      FirebaseFirestore db = FirebaseFirestore.instance;
+      final productCollection = db.collection("products");
+      var snapshot = await productCollection.where("name", isEqualTo: name).get();
+      if (snapshot.docs.isNotEmpty) {
+        for (var doc in snapshot.docs) {
+          await productCollection.doc(doc.id).delete();
+        }
+        print("Product deleted successfully.");
+      } else {
+        print("No product found with the given name.");
+      }
+    } catch (e) {
+      print("Error deleting product: $e");
+    }
+  }
+
+
 }
