1ccb9ca4ba98e633198c92a3cc879c7567656cf8
nadao-1707 <princeegypt28@gmail.com>
2024-06-21 15:22:46 +0300

    ...

--- cosmo_care/lib/Entities/Product.dart
+++ cosmo_care/lib/Entities/Product.dart
@@ -1,36 +1,50 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Product{
-
+class Product {
   final String? name;
   final int? price;
   final String? description;
   final int? code;
-  final List<String>? ingrediants;
+  final List<String>? ingredients;
+  double? averageRating; // Average rating of the product
+  int? totalRatings; // Total number of ratings
+  List<String>? reviews; // List of reviews
 
-  Product({required this.name,required this.price,required this.description,required this.code,required this.ingrediants});
+  Product({
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.code,
+    required this.ingredients,
+    this.averageRating,
+    this.totalRatings,
+    this.reviews,
+  });
 
-  factory Product.fromFirestore(
-      DocumentSnapshot<Map<String, dynamic>> snapshot,
-      SnapshotOptions? options,) {
+  factory Product.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot) {
     final data = snapshot.data();
-    return Product (
+    return Product(
       name: data?['name'],
       price: data?['price'],
       description: data?['description'],
       code: data?['code'],
-      ingrediants:
-          data?['ingrediants'] is Iterable ? List.from(data?['ingrediants']) : null,
+      ingredients: data?['ingredients'] is List ? List<String>.from(data?['ingredients']) : null,
+      averageRating: (data?['averageRating'] ?? 0.0).toDouble(),
+      totalRatings: data?['totalRatings'],
+      reviews: data?['reviews'] is List ? List<String>.from(data?['reviews']) : null,
     );
   }
-  
+
   Map<String, dynamic> toFirestore() {
     return {
       if (name != null) "name": name,
       if (price != null) "price": price,
       if (description != null) "description": description,
       if (code != null) "code": code,
-      if (ingrediants != null) "ingrediants": ingrediants, 
+      if (ingredients != null) "ingredients": ingredients,
+      "averageRating": averageRating ?? 0.0, 
+      "totalRatings": totalRatings ?? 0,
+      if (reviews != null) "reviews": reviews,
     };
   }
 }
--- cosmo_care/lib/Services/ClientController.dart
+++ cosmo_care/lib/Services/ClientController.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cosmo_care/Entities/Cart.dart';
 import 'package:cosmo_care/Entities/Client.dart';
+import 'package:cosmo_care/Entities/Product.dart';
 import 'package:cosmo_care/Services/AuthService.dart';
 
 class ClientController {
@@ -101,8 +102,63 @@ class ClientController {
   } catch (error) {
     print("Failed to remove product from cart: $error");
   }
-}
+  }
 
+  // add review and rating (update average and total rating)
+  Future<void> addRatingAndReview(String productId, int rating, String review) async {
+    try {
+      final CollectionReference<Product> _productsRef = FirebaseFirestore.instance
+      .collection('products')
+      .withConverter<Product>(
+      fromFirestore: (snapshot, _) => Product.fromFirestore(snapshot),
+      toFirestore: (product, _) => product.toFirestore(),
+      );
+      DocumentReference<Product> productDocRef = _productsRef.doc(productId);
 
-  
+      DocumentSnapshot<Product> snapshot = await productDocRef.get();
+
+      if (snapshot.exists) {
+        Product product = snapshot.data()!;
+        if (product.reviews == null) {
+          product.reviews = [];
+        }
+        product.reviews!.add(review);
+
+        double newAverageRating = ((product.averageRating ?? 0) * (product.totalRatings ?? 0) + rating) /
+            ((product.totalRatings ?? 0) + 1);
+
+        product.averageRating = newAverageRating;
+        product.totalRatings = (product.totalRatings ?? 0) + 1;
+
+        await productDocRef.set(product);
+      } else {
+        throw Exception('Product not found.');
+      }
+    } catch (error) {
+      print('Failed to add rating and review: $error');
+      rethrow;
+    }
+  }
+
+  // retrieve all the product info
+  Future<Product?> getProduct(String productId) async {
+    try {
+      final CollectionReference<Product> _productsRef =
+      FirebaseFirestore.instance.collection('products').withConverter<Product>(
+            fromFirestore: (snapshot, _) => Product.fromFirestore(snapshot),
+            toFirestore: (product, _) => product.toFirestore(),
+          );
+      DocumentReference<Product> productDocRef = _productsRef.doc(productId);
+      DocumentSnapshot<Product> snapshot = await productDocRef.get();
+      if (snapshot.exists) {
+        return snapshot.data();
+      } else {
+        print('Product not found');
+        return null;
+      }
+    } catch (error) {
+      print('Failed to fetch product: $error');
+      return null;
+    }
+  }
 }
