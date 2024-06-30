4e308ff4dc7a71edcee9d7f30b5d77e8212c9bf2
Mirna hatem <85480715+mirna-hatemm@users.noreply.github.com>
2024-07-01 00:36:39 +0300

    Updated MyProfile page

--- cosmo_care/lib/Pages/MyProfile.dart
+++ cosmo_care/lib/Pages/MyProfile.dart
@@ -1,20 +1,249 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/Pages/BarCodeScanning.dart';
+import 'package:flutter_application_2/Pages/ChatBot.dart';
+import 'package:flutter_application_2/Pages/Home.dart';
+import 'package:flutter_application_2/Pages/MyCart.dart';
+import 'package:flutter_application_2/Pages/Search.dart';
+import 'package:flutter_application_2/Pages/EditProfile.dart'; // Import the EditProfile page
+import 'package:flutter_application_2/Pages/LogIn.dart'; // Import the LogIn page
 
-void main() {
-  runApp(const MyProfile());
+class MyProfile extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyProfile extends StatelessWidget {
-  const MyProfile({super.key});
+class _HomePageState extends State<MyProfile> {
+  bool showInfo = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple, // Example theme customization
-        hintColor: Colors.pink, // Example theme customization
+    return Scaffold(
+      backgroundColor: Color(0xFFD1C4E9), // Background color
+      appBar: AppBar(
+        backgroundColor: Color(0xFFE1BEE7),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous page
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyProfile()),
+              );
+            },
+          ),
+        ],
       ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Anna Albert',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                height: 300, // Adjust height as needed
+                decoration: BoxDecoration(
+                  color: Color(0xFFEDE7F6),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.person, color: Colors.black54),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text(
+                              'Anna Albert',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.email, color: Colors.black54),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text(
+                              'info@yahoo.com',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.remove_red_eye, color: Colors.black54),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text(
+                              '********',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EditProfile()), // Navigate to EditProfile page
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFD1C4E9),
+                  foregroundColor: Colors.black,
+                ),
+                child: Text('Show Info'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  _showLogoutDialog(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFD1C4E9),
+                  foregroundColor: Colors.black,
+                ),
+                child: Text('Log Out'),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.black54,
+        backgroundColor: Color(0xFFE1BEE7),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chatbot',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home()),
+              );
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatBot()),
+              );
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BarCodeScanning()),
+              );
+              break;
+            case 3:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyCart()),
+              );
+              break;
+            case 4:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Search()),
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Log Out'),
+          content: Text('Are you sure you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => LogIn()), // Navigate to LogIn page
+                  (Route<dynamic> route) => false,
+                );
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
